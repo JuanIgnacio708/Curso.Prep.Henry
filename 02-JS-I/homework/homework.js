0386c8ce5a817a97f262690d2dfefeb8b15ad7ab
Juan Ignacio <Juan Ignacio>
2021-11-01 11:51:03 -0300

    finish homework 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -229,13 +229,14 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
+  console.log("Soy " + nombre + " " + apellido + "!");
   
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código:
+  // Tu código: 
   
 }
 
