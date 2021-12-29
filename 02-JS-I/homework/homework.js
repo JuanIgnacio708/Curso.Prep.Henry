b9029d07967e5d06de51207076d40774ac75cb5a
Juan Ignacio <Juan Ignacio>
2021-12-29 11:31:53 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -236,7 +236,8 @@ function combinarNombres(nombre, apellido) {
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
-  // Tu código: 
+  // Tu código:  
+  console.log 
   
 }
 
